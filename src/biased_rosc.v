874db381eef67ef56e719eec985d1c67c1f53caf
ThomasPluck <tompluck@mail.com>
2023-09-04 19:12:58 +0100

    fixed loop generation

--- src/biased_rosc.v
+++ src/biased_rosc.v
@@ -1,17 +1,24 @@
 `timescale 1ns/10ps
 
-module ring_osc(input ena,output osc);
+module ring_osc(input ena, output osc);
 
-  localparam NUM_INVERTERS = 150; //  must be an even number
-  
-  // setup loop of inverters
-  // http://svn.clairexen.net/handicraft/2015/ringosc/ringosc.v
-  wire [NUM_INVERTERS-1:0] delay_in, delay_out;
-  wire osc_out;
-  sky130_fd_sc_hd__inv_1 idelay [NUM_INVERTERS-1:0] (.A(delay_in),.Y(delay_out));
-  assign delay_in = {delay_out[NUM_INVERTERS-2:0], osc_out};
-  sky130_fd_sc_hd__nand2_1 nand(.A(ena),.B(delay_out[NUM_INVERTERS-1]),.Y(osc_out));
-  assign osc = osc_out;
+    localparam NUM_INVERTERS = 150; // must be an even number
+
+    // setup loop of inverters
+    wire [NUM_INVERTERS-1:0] delay_in, delay_out;
+    wire osc_out;
+
+    genvar i;
+    generate
+        for (i = 0; i < NUM_INVERTERS; i = i + 1) begin : inv_gen
+            sky130_fd_sc_hd__inv_1 idelay (.A(delay_in[i]), .Y(delay_out[i]));
+        end
+    endgenerate
+
+    assign delay_in = {delay_out[NUM_INVERTERS-2:0], osc_out};
+    sky130_fd_sc_hd__nand2_1 nand(.A(ena), .B(delay_out[NUM_INVERTERS-1]), .Y(osc_out));
+    assign osc = osc_out;
+
 endmodule
 
 module multiple_ring_oscillators#(
